4c4c0dc186658fe7af13644987afe1574662acba
yy17839999394 <2262956873@qq.com>
2019-04-10 10:24:20 +0800

    add info

--- src/com/qianfeng/test/App.java
+++ src/com/qianfeng/test/App.java
@@ -3,5 +3,7 @@ package com.qianfeng.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("你是我的小可爱嘛");
+        System.out.println("你不是我的小可爱");
+        System.out.println("巴啦啦能量");
     }
 }
